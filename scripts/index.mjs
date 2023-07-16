aa1ebcf9a5acee80a19a77d2ea81ca81a1306fbc
Espen Klem <espen.klem@gmail.com>
2023-07-16 09:17:51 +0200

    swapped to words-n-numbers emojisCustom extractor

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,12 +1,10 @@
 import fetch from 'node-fetch'
-import { extract } from 'words-n-numbers'
-
+import { extract, emojisCustom } from 'words-n-numbers'
 
 const regexCurrentVersion = '(?<=v)\\d+.\\d'
-const regexUnicode = '.+(?=;)'
+const regexUnicode = '.+(?= ;)'
 const regexVersion = '(?<=; )\\d+.\\d'
-const regexEmoji = '(?<=#\\s).'
-const regexEmojiDescription = '(?<=#\\s.+\\s)[\\w-\’ \\s:,]+' // <-- This needs gui-flags as options since the regular expression is dependent on unicode flag not present
+const regexEmojiDescription = '(?<=#\\s.+\\s)[\\w-’ \\s:,]+'
 
 const checkEmojiLine = function(line) {
   if (line.startsWith('#')) {
@@ -33,14 +31,13 @@ console.log(currentVersion)
 
 // go through a line with emoji and extract
 if (checkEmojiLine(unicodeText[5])) {
-  const unicode = extract(unicodeText[5], { regex: regexUnicode })
-  const version = extract(unicodeText[5], { regex: regexVersion })
-  const emoji = extract(unicodeText[5], { regex: regexEmoji })
-  const emojiDescription = extract(unicodeText[5], { regex: regexEmojiDescription, flags: 'gi' })
+  let unicode = extract(unicodeText[2051], { regex: regexUnicode })
+  unicode = unicode[0].split(' ')
+  const version = extract(unicodeText[2051], { regex: regexVersion })
+  const emoji = extract(unicodeText[2051], { regex: emojisCustom, flags: 'g' })
+  const emojiDescription = extract(unicodeText[2051], { regex: regexEmojiDescription , flags: 'g' })
   console.log(unicode)
   console.log(version)
   console.log(emoji)
   console.log(emojiDescription)
 }
-
-

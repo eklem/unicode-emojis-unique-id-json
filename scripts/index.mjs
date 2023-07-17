5ba204cfd47403f21a744e136677f03e79cb3c61
Espen Klem <espen.klem@gmail.com>
2023-07-17 08:45:56 +0200

    Getting text file converted to array of objects

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -14,30 +14,65 @@ const checkEmojiLine = function(line) {
   }
 }
 
-
 // Fetch unicode emojis
 const response = await fetch('https://www.unicode.org/emoji/charts/emoji-ordering.txt')
 const body = await response.text()
 // console.log(body)
 
 // split text to array
-const unicodeText = body.split('\n')
-// console.dir(unicodeText)
+const emojiText = body.split('\n')
+// console.dir(emojiText)
 
 // find version of Unicode Emoji set
-const currentVersion = extract(unicodeText[0], { regex: regexCurrentVersion })[0]
+const currentVersion = extract(emojiText[0], { regex: regexCurrentVersion })[0]
 console.log(currentVersion)
 
-
-// go through a line with emoji and extract
-if (checkEmojiLine(unicodeText[5])) {
-  let unicode = extract(unicodeText[2051], { regex: regexUnicode })
-  unicode = unicode[0].split(' ')
-  const version = extract(unicodeText[2051], { regex: regexVersion })
-  const emoji = extract(unicodeText[2051], { regex: emojisCustom, flags: 'g' })
-  const emojiDescription = extract(unicodeText[2051], { regex: regexEmojiDescription , flags: 'g' })
-  console.log(unicode)
-  console.log(version)
-  console.log(emoji)
-  console.log(emojiDescription)
+// go through a line with emoji, extract and return array of objects
+const createEmojisArray = function (emojiText) {
+  console.log(emojiText)
+  const emojisArray = []
+  emojiText.forEach(emojiLine => {
+    console.log(emojiLine)
+    if (checkEmojiLine(emojiLine)) {
+      let unicode = extract(emojiLine, { regex: regexUnicode })
+      if (unicode === null) {
+        return
+      }
+      unicode = unicode[0].split(' ')
+      const version = extract(emojiLine, { regex: regexVersion })
+      const emoji = extract(emojiLine, { regex: emojisCustom, flags: 'g' })
+      if (emoji === null) {
+        return
+      }
+      const emojiDescription = extract(emojiLine, { regex: regexEmojiDescription , flags: 'g' })
+      console.log(unicode)
+      console.log(version)
+      console.log(emoji)
+      console.log(emojiDescription)
+      // create object
+      const emojiObj = {
+        unicode: unicode,
+        version: version,
+        emoji: emoji,
+        emojiDescription: emojiDescription
+      }
+      emojisArray.push(emojiObj)
+    }
+  })
+  console.log(emojisArray.length)
+  return emojisArray
 }
+
+createEmojisArray(emojiText)
+
+// if (checkEmojiLine(unicodeText[5])) {
+//   let unicode = extract(unicodeText[2051], { regex: regexUnicode })
+//   unicode = unicode[0].split(' ')
+//   const version = extract(unicodeText[2051], { regex: regexVersion })
+//   const emoji = extract(unicodeText[2051], { regex: emojisCustom, flags: 'g' })
+//   const emojiDescription = extract(unicodeText[2051], { regex: regexEmojiDescription , flags: 'g' })
+//   console.log(unicode)
+//   console.log(version)
+//   console.log(emoji)
+//   console.log(emojiDescription)
+// }

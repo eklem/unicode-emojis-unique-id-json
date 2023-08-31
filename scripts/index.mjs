6e22fab4187ba134e971c9a2562589bdff9f0df0
Espen Klem <espen.klem@gmail.com>
2023-08-31 13:16:40 +0200

    reading from correct file scraping all the fully-qualified emojis - it's 3655 of them in unicode emojis v15.0

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,10 +1,11 @@
 import fetch from 'node-fetch'
 import { extract, emojisCustom } from 'words-n-numbers'
 
-const regexCurrentVersion = '(?<=v)\\d+.\\d'
-const regexUnicode = '.+(?= ;)'
-const regexVersion = '(?<=; )\\d+.\\d'
-const regexEmojiDescription = '(?<=#\\s.+\\s)[\\w-’ \\s:,]+'
+const regexCurrentVersion = '(?<=: )\\d+.\\d'
+const regexUnicode = '[\\d\\w\\s]+(?=\\b\\s+;)'
+const regexVersion = 'E\\d+.\\d '
+const regexQualification = '(?<=; )([\\w-])+'
+const regexEmojiDescription = '(?<=E\\d+.\\d ).+'
 
 const checkIfEmojiLine = function(line) {
   if (line.startsWith('#') || (line.trim().length === 0)) {
@@ -15,7 +16,7 @@ const checkIfEmojiLine = function(line) {
 }
 
 // Fetch unicode emojis
-const response = await fetch('https://www.unicode.org/emoji/charts/emoji-ordering.txt')
+const response = await fetch('https://unicode.org/Public/emoji/15.0/emoji-test.txt')
 const body = await response.text()
 // console.log(body)
 
@@ -24,56 +25,41 @@ const emojiText = body.split('\n')
 // console.dir(emojiText)
 
 // find version of Unicode Emoji set
-const currentVersion = extract(emojiText[0], { regex: regexCurrentVersion })[0]
+const currentVersion = extract(emojiText[7], { regex: regexCurrentVersion })
 console.log(currentVersion)
 
 // go through a line with emoji, extract and return array of objects
 const createEmojisArray = function (emojiText) {
-  console.log(emojiText)
+  // console.log(emojiText)
   const emojisArray = []
   emojiText.forEach(emojiLine => {
-    // console.log('emojiLine: ' + emojiLine + '\nlength: ' + emojiLine.trim().length)
     if (checkIfEmojiLine(emojiLine)) {
+      // regex content of each emohi line
       let unicode = extract(emojiLine, { regex: regexUnicode })
-        // if (unicode === null) {
-        //   return
-        // }
       unicode = unicode[0].split(' ')
       const version = extract(emojiLine, { regex: regexVersion })
       const emoji = extract(emojiLine, { regex: emojisCustom, flags: 'g' })
-      // skip iteration if emoji isn't extracted
-      if (emoji === null) {
+      const qualified = extract(emojiLine, { regex: regexQualification })
+      // skip iteration if emoji isn't extracted, or...
+      // skip if other than fully-qualified (component, minimally-qualified or unqualified)
+      if (emoji === null || qualified[0] !== 'fully-qualified' ) {
         return
       }
       const emojiDescription = extract(emojiLine, { regex: regexEmojiDescription , flags: 'g' })
-      console.log(unicode)
-      console.log(version)
-      console.log(emoji)
-      console.log(emojiDescription)
       // create object
       const emojiObj = {
         unicode: unicode,
         version: version,
         emoji: emoji,
-        emojiDescription: emojiDescription
+        emojiDescription: emojiDescription,
+        qualified: qualified
       }
       emojisArray.push(emojiObj)
     }
   })
-  console.log(emojisArray.length)
   return emojisArray
 }
 
-createEmojisArray(emojiText)
-
-// if (checkEmojiLine(unicodeText[5])) {
-//   let unicode = extract(unicodeText[2051], { regex: regexUnicode })
-//   unicode = unicode[0].split(' ')
-//   const version = extract(unicodeText[2051], { regex: regexVersion })
-//   const emoji = extract(unicodeText[2051], { regex: emojisCustom, flags: 'g' })
-//   const emojiDescription = extract(unicodeText[2051], { regex: regexEmojiDescription , flags: 'g' })
-//   console.log(unicode)
-//   console.log(version)
-//   console.log(emoji)
-//   console.log(emojiDescription)
-// }
+const emojisArray = createEmojisArray(emojiText)
+console.log(emojisArray)
+console.log('Number of emojis: ' + emojisArray.length)

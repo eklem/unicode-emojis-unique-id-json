33e755fd99b586f4a9d0001ac49b60fb453086a7
Espen Klem <espen.klem@gmail.com>
2023-12-02 22:43:18 +0100

    Basic stuff working Still need to figure out how to do tests against previous versions.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # unicode-emojis-unique-id-json
 
-JSON-version of https://unicode.org/Public/emoji/15.0/emoji-test.txt with unique IDs (numbers). The IDs should be unique over time, when new versions of unicode emojis are released. There are a little less than 4000 emojis now if you count the ones with modifiers. I'm guessing it will pass 10000 in the foreseable future, so the IDs should be 5-digits.
+JSON-version of https://unicode.org/Public/emoji/15.0/emoji-test.txt with unique IDs (numbers). The IDs should be unique over time, when new versions of unicode emojis are released. There are a little less than 4000 emojis now if you count the ones with modifiers. I'm guessing it will pass 10000 in the foreseeable future, so the IDs should be 5-digits.
 
 The IDs will start at 00000 and then counting. The reasons for this way of creating IDs instead of some hashing algorithm are two:
 
@@ -34,7 +34,8 @@ will be:
 ## Work to be done
 
 * [x] regex for extracting content from text-file.
-* [ ] write to JSON
+* [x] read old JSON, fetch new emojis, convert to JSON and add what's not in the old JSON with unique IDs
+* [x] write to JSON
 * [ ] show which unicode emoji versions are met with this library
 * [ ] tests to check that all IDs for previous versions of this library are corresponding to new version of library. Do this by having a previous version directory
 
--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,6 +1,8 @@
 import fetch from 'node-fetch'
 import { extract, emojisCustom } from 'words-n-numbers'
 import { existsSync, readFileSync, writeFileSync } from 'fs'
+const file = '../dist/unicode-emojis-unique-id.json'
+const emojiURL = 'https://unicode.org/Public/emoji/15.1/emoji-test.txt'
 
 // #########################################################################################
 // A: Regular expressions to extract emojis + extra info on each emoji line
@@ -8,7 +10,7 @@ const regexCurrentVersion = '(?<=: )\\d+.\\d'
 const regexUnicode = '[\\d\\w\\s]+(?=\\b\\s+;)'
 const regexVersion = 'E\\d+[.]\\d'
 const regexQualification = '(?<=; )([\\w-])+'
-const regexEmojiDescription = '(?<=E\\d+[.]\\d ).+'
+const regexDescription = '(?<=E\\d+[.]\\d ).+'
 
 // #########################################################################################
 // B: Function: Check if a line is a comment and split into lines
@@ -55,11 +57,12 @@ const createEmojiArray = function (emojiText) {
       if (emoji === null || qualified[0] !== 'fully-qualified' ) {
         return
       }
-      const emojiDescription = extract(emojiLine, { regex: regexEmojiDescription , flags: 'g' })
+      const description = extract(emojiLine, { regex: regexDescription , flags: 'g' })
       // create object
       const emojiObj = {
+        id: '',
         emoji: emoji[0],
-        emojiDescription: emojiDescription[0],
+        description: description[0],
         unicode: unicode,
         versionIntroduced: version[0]
         // qualified: qualified[0]
@@ -73,33 +76,66 @@ const createEmojiArray = function (emojiText) {
 
 // #########################################################################################
 // E: Read existing JSON file, compare and update
-const readFileAndUpdate = function () {
-  if (existsSync('../dist/unicode-emojis-unique-id.json', 'utf8')) {
+const readFile = function (file) {
+  if (existsSync(file, 'utf8')) {
     console.log('file exists')
-    const emojisUniqueId = readFileSync('../dist/unicode-emojis-unique-id.json', 'utf8').toString().trim()
-    console.log(JSON.parse(emojisUniqueId))
+    const emojisUniqueId = readFileSync(file, 'utf8').toString().trim()
+    const fileJSON = JSON.parse(emojisUniqueId)
+    return fileJSON
   } else {
     console.log('File doesn\'t exist\nYou need to create one with at least one emoji object in an array')
+    // File starter:
+    // {
+    //   "unicode-emojis-version": "15.0",
+    //   "emojis": [
+    //     {
+    //       "id": "00001",
+    //       "emoji": "😃",
+    //       "description": "grinning face with big eyes",
+    //       "unicode": ["U+1F603"],
+    //       "versionIntroduced": "0.6"
+    //     }
+    //   ]
+    // }
   }
 }
 
 // #########################################################################################
 // F: Read local JSON, fetch Unicode.org JSON and build new JSON to write
-// 
+//    1: Get highest ID from read array of emojis
+//    2: Loop through fetched array
+//    3: Check if emoji object is present by comparing obj.unicode
+//    4: If not, add ID set to read array.length + 1 (counting starts at 1) and pad it to be 5 digits long
+//    5: Add object to array
 
+const addNewObjects = function (readJSON, fetchedJSON, unicodeVersion) {
+  if (readJSON.version === unicodeVersion) {
+    console.log('local file up to date with Unicode Emojis version: ' + unicodeVersion)
+  } else {
+    console.log('local file needs to be updated with lates Unicode Emojis')
+    console.log('existing array length: ' + readJSON.emojis.length)
+
+    // Loop through new array and add ID
+    for (const obj of fetchedJSON) {
+      if (readJSON.emojis.some((existingObj) => existingObj.description === obj.description )) {
+        console.log('already existing, skipping')
+      } else {
+        // console.log('new emoji, do the work')
+        const idNum = readJSON.emojis.length + 1
+        obj.id = JSON.stringify(idNum).padStart(5, '0')
+        readJSON.emojis.push(obj)
+      }
+    }
+    return readJSON
+  }
+}
 
 // #########################################################################################
 // Y: Do stuff
-const emojiText = await getEmojiFile('https://unicode.org/Public/emoji/15.0/emoji-test.txt')
-const emojiArray = createEmojiArray(emojiText)
-const unicodeEmojiVersion = getUnicodeEmojiVersion(emojiText[7])
-// console.log(emojiText)
-console.log('Number of emojis: ' + emojiArray.length)
-console.log('Unicode emoji version: ' + unicodeEmojiVersion)
-readFileAndUpdate()
-
-
-// #########################################################################################
-// Z: Stuff to be done:
-//    * Compare fetched and read JSON to see if they are the same OR check Unicode emoji version
-//    * 
+let fileJSON = readFile(file)
+const emojiText = await getEmojiFile(emojiURL)
+const unicodeVersion = getUnicodeEmojiVersion(emojiText[7])
+const unicodeJSON = createEmojiArray(emojiText)
+let newFile = addNewObjects(fileJSON, unicodeJSON, unicodeVersion)
+console.log(newFile)
+writeFileSync(file, JSON.stringify(newFile, null, '  '), 'utf8')
